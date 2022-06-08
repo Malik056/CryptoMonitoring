046fc89bf8f737646dac27d3938c9fbce75a641c
Muhammad Taha <tahamahmoodmalik@gmail.com>
2022-06-08 19:49:03 +0500

    added link to test page

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import { onMounted } from "@vue/runtime-core";
 import Web3 from "web3";
 
 export default {
@@ -14,102 +13,104 @@ export default {
   },
 
   async created() {
-    const jsonInterface = [
-      {
-        inputs: [],
-        name: "getAge",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [],
-        name: "getHeight",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [],
-        name: "getWeight",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "x",
-            type: "uint256",
-          },
-        ],
-        name: "setAge",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "y",
-            type: "uint256",
-          },
-        ],
-        name: "setHeight",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "z",
-            type: "uint256",
-          },
-        ],
-        name: "setWeight",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ];
-
-    const web3 = new Web3("http://5.28.41.146:9545");
-    const myContract = new web3.eth.Contract(
-      jsonInterface,
-      "0x92ad979f7Fd9aff973b242A907D2e1f49AF50117"
-    );
-
-    // const owner = myContract.owner.call();
-    // this.owner = owner;
-    console.log("MyContract: ", myContract);
-    this.age = await myContract.methods.getAge().call();
-    this.height = await myContract.methods.getHeight().call();
-    this.weight = await myContract.methods.getWeight().call();
-    console.log(this.age);
-    console.log(this.weight);
-    console.log(this.height);
+    try {
+      const jsonInterface = [
+        {
+          inputs: [],
+          name: "getAge",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "getHeight",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "getWeight",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "x",
+              type: "uint256",
+            },
+          ],
+          name: "setAge",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "y",
+              type: "uint256",
+            },
+          ],
+          name: "setHeight",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "z",
+              type: "uint256",
+            },
+          ],
+          name: "setWeight",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+      console.log("created");
+      const web3 = new Web3("http://5.28.41.146:9545");
+      const myContract = new web3.eth.Contract(
+        jsonInterface,
+        "0x92ad979f7Fd9aff973b242A907D2e1f49AF50117"
+      ); // const owner = myContract.owner.call();
+      // this.owner = owner;
+      console.log("MyContract: ", myContract);
+      this.age = await myContract.methods.getAge().call();
+      this.height = await myContract.methods.getHeight().call();
+      this.weight = await myContract.methods.getWeight().call();
+      console.log(this.age);
+      console.log(this.weight);
+      console.log(this.height);
+    } catch (ex) {
+      console.log(ex);
+    }
   },
 };
 </script>
